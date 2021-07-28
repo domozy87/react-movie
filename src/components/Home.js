b31a380e97e2f8d94f077912609bc64eb105f13d
Domozy <domozy@Domozys-MBP.JOOWIN>
2021-07-28 14:24:18 -0300

    Add Thumb components

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 import HeroImage from './HeroImage';
 import Grid from './Grid';
+import Thumb from './Thumb'
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -26,11 +27,17 @@ const Home = () => {
                     title={state.results[0].original_title}
                     text={state.results[0].overview} />
                 : null
-
             }
             <Grid header='Popular Movies'>
                 {state.results.map( movie => (
-                    <div key={movie.id}>{movie.title}</div>
+                    <Thumb
+                        key={movie.id}
+                        clickable
+                        image={
+                            movie.poster_path ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path : NoImage
+                        }
+                        movieId={movie.id}
+                    />
                 ))}
             </Grid>
         </>
