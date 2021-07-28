6e7a0869acf3dd13ad1e07d61d818dc30397a3f5
Domozy <domozy@Domozys-MBP.JOOWIN>
2021-07-28 15:50:49 -0300

    Add load more button

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 // Style
 import { Spinner } from './Spinner.styles';
 
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -10,10 +10,10 @@ const initialState = {
 };
 
 export const useHomeFetch = () => {
+    const [searchTerm, setSearchTerm] = useState('');
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('');
 
     const fetchMovies = async (page, searchTerm = '') => {
         try {
@@ -34,10 +34,11 @@ export const useHomeFetch = () => {
         setLoading(false);
     };
 
-    // Initialize rendering
+    // Initialize rendering and search
     useEffect(() => {
-        fetchMovies(1);
-    }, []);
+        setState(initialState);
+        fetchMovies(1, searchTerm);
+    }, [searchTerm]);
 
     return { state, loading, error, setSearchTerm };
 };
