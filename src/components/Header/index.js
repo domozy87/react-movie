431c5f92293841b912b49e72961b860ad4ccabd7
Domozy <domozy@Domozys-MBP.JOOWIN>
2021-07-28 16:39:31 -0300

    Add default movie page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb-logo.svg'
 
+import { Link } from 'react-router-dom';
+
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
 
 const Header = () => (
     <Wrapper>
         <Content>
-            <LogoImg src={RMDBLogo} alt='react-movie-logo' />
+            <Link to='/'>
+                <LogoImg src={RMDBLogo} alt='react-movie-logo' />
+            </Link>
             <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo' />
         </Content>
     </Wrapper>
---
+++ src/components/Movie.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+
+// Components
+import Grid from './Grid';
+import Spinner from './Spinner';
+
+// Hooks
+
+
+// Image
+import NoImage from '../images/no-image.jpeg';
+
+const Movie = () => {
+
+    return (
+        <div>Movie</div>
+    );
+}
+
+export default Movie;
