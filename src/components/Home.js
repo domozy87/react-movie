bd7bc5431e2fc5e940982c4b28eab4e7d6c978da
Domozy <domozy@Domozys-MBP.JOOWIN>
2021-07-28 12:19:05 -0300

    Add Grid component for popular movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 // Components
+import HeroImage from './HeroImage';
+import Grid from './Grid';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -15,10 +17,24 @@ import NoImage from '../images/no-image.jpeg';
 
 const Home = () => {
     const { state, loading, error } = useHomeFetch();
-
-    console.log(state);
     
-    return <div>Home Page</div>
+    return (
+        <>
+            {state.results[0] ?
+                <HeroImage
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview} />
+                : null
+
+            }
+            <Grid header='Popular Movies'>
+                {state.results.map( movie => (
+                    <div key={movie.id}>{movie.title}</div>
+                ))}
+            </Grid>
+        </>
+    );
 };
 
 export default Home;
