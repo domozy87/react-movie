3cca4acc1d5ec6e47295f60584233a7cd675c318
Domozy <domozy@Domozys-MBP.JOOWIN>
2021-07-28 22:40:03 -0300

    Add Actor component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,6 +9,7 @@ import Spinner from './Spinner';
 import BreadCrumb from './BreadCrumb';
 import MovieInfo from './MovieInfo';
 import MovieInfoBar from './MovieInfoBar';
+import Actor from './Actor';
 
 // Hooks
 import { useParams } from 'react-router-dom';
@@ -38,6 +39,18 @@ const Movie = () => {
                 budget={movie.budget}
                 revenue={movie.revenue}
             />
+            <Grid header="Actors">
+                {movie.actors.map( actor => (
+                    <Actor 
+                        key={actor.credit_id}
+                        name={actor.name}
+                        character={actor.character}
+                        imageUrl={
+                            actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage
+                        }
+                    />
+                ) )}
+            </Grid>
         </>
     );
 }
