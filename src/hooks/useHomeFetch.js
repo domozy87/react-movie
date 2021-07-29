ff3c1ec7d0835909f94c3d5b56976c1ebc681d78
Domozy <domozy@Domozys-MBP.JOOWIN>
2021-07-28 23:40:37 -0300

    Save State into sessionStorage

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -2,6 +2,9 @@ import {useState, useEffect, useRef} from 'react';
 
 import API from '../API';
 
+// Helper
+import { isPersistedState } from '../helpers';
+
 const initialState = {
     page: 0,
     results: [],
@@ -37,8 +40,18 @@ export const useHomeFetch = () => {
 
     // Initialize rendering and search
     useEffect(() => {
-        setState(initialState);
-        fetchMovies(1, searchTerm);
+        if ( !searchTerm ) {
+            const sessionState = isPersistedState('homeState');
+
+            if ( sessionState ) {
+                setState( sessionState );
+
+                return;
+            }
+        }
+
+        setState( initialState );
+        fetchMovies( 1, searchTerm );
     }, [searchTerm]);
 
     // Load more
@@ -50,6 +63,13 @@ export const useHomeFetch = () => {
         fetchMovies(state.page + 1, searchTerm);
         setLoadingMore(false);
     }, [isLoadingMore, searchTerm, state.page] );
+
+    // Write to session storage
+    useEffect( () => {
+        if ( !searchTerm ) {
+            sessionStorage.setItem( 'homeState', JSON.stringify(state) );
+        }
+    }, [searchTerm, state] );
 
     return { state, loading, error, searchTerm, setSearchTerm, isLoadingMore, setLoadingMore };
 };
